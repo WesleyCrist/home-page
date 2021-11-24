063a509d5421e3702efdb311d62945be0d5fbb4b
WesleyCris <wesleycristiano@outlook.com>
2021-11-24 19:39:54 -0300

    Adequação na estrutura do html e correção de erro de calculo imc no js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var nome
-var num, idade, peso, altura, IMC
+var idade, peso, altura, tmp, IMC
 
 alert("Bem vindo!")
 
 nome = prompt("Qual seu nome?")
 idade = Number.parseInt(prompt("Qual sua idade?"))
 peso = Number.parseFloat(prompt("Qual seu peso?"))
-altura = Number.parseFloat(prompt("Qual sua altura?"))
+tmp = Number.parseFloat(prompt("Qual sua altura em centímetros?"))
 
+altura = tmp / 100
 IMC = peso / (altura * altura)
 
 if (idade >= 65) {
@@ -15,7 +16,7 @@ if (idade >= 65) {
     if (IMC <= 22){
         alert(nome + ", você está abaixo do peso!")
     }
-    else if (IMC > 22 || IMC <= 27){
+    else if (IMC > 22 && IMC <= 27){
         alert(nome + ", você está adequado ou eutrófico!")
     }
     else {
@@ -28,16 +29,16 @@ else if (idade < 65) {
     if (IMC < 18.5) {
         alert(nome + ", você está abaixo do peso!")
     }
-    else if (IMC >=18.5 || IMC < 24.9) {
+    else if (IMC >=18.5 && IMC < 24.9) {
         alert(nome + ", você está com o peso normal!")
     }
-    else if (IMC >=25 || IMC < 29.9) {
+    else if (IMC >=25 && IMC < 29.9) {
         alert(nome + ", excesso de peso!")
     }
-    else if (IMC >=30 || IMC < 34.9) {
+    else if (IMC >=30 && IMC < 34.9) {
         alert(nome + ", você está com obesidade classe 1!")
     }
-    else if (IMC >= 35 || IMC < 39.9) {
+    else if (IMC >= 35 && IMC < 39.9) {
         alert(nome + ", você está com obesidade classe 2!")
     }
     else {
